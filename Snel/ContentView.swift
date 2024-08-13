4fe4ff00131a51c6960e580ca9ac63d675e90871
Jordi Bruin <jordi@goodsnooze.com>
2024-08-13 22:48:50 +0200

    Initial Commit

---
+++ Snel/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Snel
+//
+//  Created by Jordi Bruin on 13/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
